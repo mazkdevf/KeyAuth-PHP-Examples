c6726662d967dc1101afa3e5e3ce0135a2f1865f
mazkdevf <79049205+mazk5145@users.noreply.github.com>
2021-11-28 20:55:58 +0200

    28.11 November 20 API

---
+++ User-Pass-Version/keyauth.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace KeyAuth;
+
+session_start();
+
+class api {
+    public $name, $ownerid;
+
+    function __construct($name, $ownerid) {
+        $this->name = $name;
+        $this->ownerid = $ownerid;
+    }
+	
+	function init(){
+        $data = array(
+            "type" => "init",
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+            $_SESSION['sessionid'] = $json->sessionid;
+    }
+
+    function login($username,$password){
+        $data = array(
+            "type" => "login",
+            "username" => $username,
+            "pass" => $password,
+            "sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+        // MODIFED FROM ORIGINAL
+            $_SESSION["user_data"] = (array)$json;
+            $_SESSION["substatus"] = $json->success;
+            $_SESSION["submessage"] = $json->message;
+            $_SESSION["userdata"] = (array)$json->info;
+
+        return $json->success;
+    }
+	
+	function register($username,$password,$key){
+        $data = array(
+            "type" => "register",
+            "username" => $username,
+            "pass" => $password,
+            "key" => $key,
+            "sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+            $_SESSION["user_data"] = (array)$json->info;
+
+        return $json->success;
+    }
+	
+	function license($key){
+        $data = array(
+            "type" => "license",
+            "key" => $key,
+            "sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+        // MODIFED FROM ORIGINAL
+            $_SESSION["user_data"] = (array)$json;
+            $_SESSION["substatus"] = $json->success;
+            $_SESSION["submessage"] = $json->message;
+            $_SESSION["userdata"] = (array)$json->info;
+
+        return $json->success;
+    }
+	
+	function upgrade($username,$key){
+        $data = array(
+            "type" => "upgrade",
+            "username" => $username,
+            "key" => $key,
+            "sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+            $_SESSION["user_data"] = (array)$json->info;
+
+        return $json->success;
+    }
+	
+	function var($varid){
+        $data = array(
+            "type" => "var",
+            "varid" => $varid,
+            "sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $response = $this->req($data);
+
+        $json = json_decode($response);
+
+        if(!$json->success)
+            $this->error($json->message);
+        else if($json->success)
+            return $json->message;
+    }
+
+    function log($message){
+        $logkey = $_SESSION["user_data"]["key"] ?? "NONE";
+
+        $data = array(
+            "type" => "log",
+            "pcuser" => "Server",
+            "message" => $message,
+			"sessionid" => $_SESSION['sessionid'],
+            "name" => $this->name,
+            "ownerid" => $this->ownerid
+        );
+
+        $this->req($data);
+    }
+
+    private function req($data){
+        $curl = curl_init("https://keyauth.com/api/1.1/");
+        curl_setopt($curl, CURLOPT_USERAGENT, "KeyAuth");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $response = curl_exec($curl);
+
+        return $response;
+    }
+
+    public function error($msg){
+                        echo '
+                <script type=\'text/javascript\'>
+                
+                const notyf = new Notyf();
+                notyf
+                  .error({
+                    message: \''.$msg.'\',
+                    duration: 3500,
+                    dismissible: true
+                  });                
+                
+                </script>
+                ';  
+    }
+}
